d3bebdde6c674e9230cb0996cf6a9c3ec0b56abb
will <will.weezy324@gmail.com>
2023-12-18 13:19:28 +0900

    まだ途中です

--- exercises/easy-fizzbuzz.js
+++ exercises/easy-fizzbuzz.js
@@ -27,22 +27,17 @@ Explanation: 1 is not divisible by either 3 or 5
  * @returns String
  */
 function easyFizzBuzz(n) {
-    /*
-    Your code here
-    */
-
-
-
-
-
-
+  for (let i = 1; i < 100; i++) {
+    if (i % 3 === 0 && i % 5 == 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+  }
 }
-
-
 
 /*
 Do not alter this code
 */
-export {
-    easyFizzBuzz
-}
+export { easyFizzBuzz };
